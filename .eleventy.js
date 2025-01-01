fc24458e1a04624fb11e6b6118fd07c238bf4a4b
amitmadhraa <amitmadhraa@gmail.com>
2025-01-01 14:52:46 +0530

    Add multilingual support and all pages (About, Services, Contact)

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,8 @@
+const i18n = {
+    en: require('./src/_data/i18n/en.json'),
+    es: require('./src/_data/i18n/es.json')
+};
+
 module.exports = function(eleventyConfig) {
     // Copy the `assets` directory to the compiled site
     eleventyConfig.addPassthroughCopy("src/assets");
@@ -6,12 +11,36 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addWatchTarget("./src/assets/");
     eleventyConfig.addWatchTarget("./src/styles/");
 
+    // Add i18n shortcode
+    eleventyConfig.addShortcode("i18n", function(key, lang = 'en') {
+        return i18n[lang][key] || key;
+    });
+
+    // Add redirect shortcode
+    eleventyConfig.addShortcode("redirect", function(url) {
+        return `
+            <!DOCTYPE html>
+            <meta http-equiv="refresh" content="0; url=${url}">
+            <link rel="canonical" href="${url}">
+        `;
+    });
+
+    // Add language collection
+    eleventyConfig.addCollection("pages_en", function(collection) {
+        return collection.getFilteredByGlob("src/en/**/*.njk");
+    });
+    
+    eleventyConfig.addCollection("pages_es", function(collection) {
+        return collection.getFilteredByGlob("src/es/**/*.njk");
+    });
+
     return {
         dir: {
             input: "src",
             output: "_site",
             includes: "_includes",
-            layouts: "_layouts"
+            layouts: "_layouts",
+            data: "_data"
         },
         templateFormats: ["html", "njk", "md"],
         htmlTemplateEngine: "njk",
